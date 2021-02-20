1bd7d4ee1cca2dc449bf5dd35f398cf3e2124d91
rootuss <rootuss@op.pl>
2021-02-20 11:14:17 +0100

    add some improvements

--- src/main/java/webapp/HelloService.java
+++ src/main/java/webapp/HelloService.java
@@ -1,11 +1,15 @@
 package webapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Optional;
 
 public class HelloService {
 
     static final String FALLBACK_NAME = "world";
     static final Lang FALLBACK_LANG = new Lang(1L, "Hello", "en");
+    private final Logger logger = LoggerFactory.getLogger(HelloService.class);
 
     private LangRepository repository;
 
@@ -17,14 +21,18 @@ public class HelloService {
         this.repository = repository;
     }
 
-
     String prepareGreeting(String name) {
         return prepareGreeting(name, null);
     }
 
-
     String prepareGreeting(String name, String lang) {
-        Long langId = Optional.ofNullable(lang).map(Long::valueOf).orElse(FALLBACK_LANG.getId());
+        Long langId;
+        try {
+            langId = Optional.ofNullable(lang).map(Long::valueOf).orElse(FALLBACK_LANG.getId());
+        } catch (NumberFormatException exception) {
+            logger.warn("NON-NUMERIC LANGUAGE id USED: " + lang);
+            langId = FALLBACK_LANG.getId();
+        }
 
         String welcomeMsg = repository.findById(langId).orElse(FALLBACK_LANG).getWelcomeMsg();
         String nameToWelcome = Optional.ofNullable(name).orElse(FALLBACK_NAME);
